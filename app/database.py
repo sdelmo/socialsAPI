0f17aab9adfad4685868b6bfa7a72e756ed1debf
Sebastian Delgado <sebastiansdelgado@gmail.com>
2024-03-13 23:25:13 +0000

    refactor for ORM

--- app/database.py
+++ app/database.py
@@ -3,6 +3,23 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
-SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:q765@localhost/fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# We define our tables within python thanks to ORMS
+
+# Dependency
+# We get a session every time we get a request, and then we close it, we call it every time we get a req
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -6,28 +6,15 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-# Create instance
+from sqlalchemy.orm import Session
+from . import models, schemas
+from .database import engine, get_db
+models.Base.metadata.create_all(bind=engine)
+
+# Create instances
 
 app = FastAPI()
 
-my_posts = [
-    {"title": "post1", "content": "aaa", "id": 1},
-    {"title": "post2", "content": "bbb", "id": 2},
-    {"title": "post3", "content": "ccc", "id": 123}
-]
-
-
-def find_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return p, i
-
-
-class Post(BaseModel):
-
-    title: str
-    content: str
-    published: bool = True
 
 # Use a try/except in case conection fails
 
@@ -52,65 +39,98 @@ async def root():
 
 # Retrieve social media posts
 
+
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+
+    posts = db.query(models.Post).all()
+
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    # Create a new post
+
+    new_post = models.Post(
+        **post.model_dump())
+
+    # Add new post to db
+    db.add(new_post)
+    # Commit the new post
+    db.commit()
+
+    # Return the post i just created
+    db.refresh(new_post)
 
     return {"data": new_post}
 
 
 # Id is a path parameter, hence embedded in url
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int, response: Response, db: Session = Depends(get_db)):
 
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
 
-    found_post = cursor.fetchone()
+    # found_post = cursor.fetchone()
 
-    if not found_post:
+    # Filter is like a where statement
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} not found")
 
-    return {"post_detail": found_post}
+    return {"post_detail": post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, response: Response):
+def delete_post(id: int, response: Response, db: Session = Depends(get_db)):
 
-    cursor.execute(
-        """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
+    # cursor.execute(
+    #     """DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
 
-    deleted_post = cursor.fetchone()
-    conn.commit()
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
 
-    if not deleted_post:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    if not post_query.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} not found")
+
+    post_query.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
 
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %sRETURNING *""",
-                   (post.title, post.content, post.published, (str(id))))
-    updated_post = cursor.fetchone()
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %sRETURNING *""",
+    #                (post.title, post.content, post.published, (str(id))))
+    # updated_post = cursor.fetchone()
 
-    conn.commit()
+    # conn.commit()
 
-    if not updated_post:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    acc_post = post_query.first()
+
+    if not acc_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} not found")
 
-    return {"data": updated_post}
+    post_query.update(post.model_dump(), synchronize_session=False)
+
+    db.commit()
+
+    return {"data": post_query.first()}
